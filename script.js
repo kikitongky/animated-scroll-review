8c55109ebd8dc2f3cca7a7898ba640263ed92180
kikitongky <tongkytongky07@gmail.com>
2023-12-23 21:28:15 +0700

    commit pertama kali

---
+++ script.js
@@ -0,0 +1,24 @@
+gsap.registerPlugin(ScrollTrigger); 
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".reviews",
+        start: "top 40%",
+        end: "bottom top",
+        scrub: 1,
+        markers: true,
+    }
+});
+
+tl.fromTo(".rl", {y: '350%'}, {y: '-150%'}, 0);
+tl.fromTo(".rr", {y: '300%'}, {y: '-50%'}, 0);
+
+gsap.fromTo(".rtitle", {x: '100%'}, {x:'-120%', scrollTrigger: {
+    trigger: ".rtitle",
+    start: "center center",
+    end: "bottom center",
+    endTrigger: ".reviews",
+    pin: true,
+    scrub: 1,
+    markers: true,
+}},0);
